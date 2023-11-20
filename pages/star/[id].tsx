3055d6d0e4eac4d487f8b44c9084d5454c42d205
TheMoi <35933570+TheMoi@users.noreply.github.com>
2023-11-20 16:29:19 +0100

    Wikidata Star Front
    Les données Wikidata s'affichent

--- pages/star/[id].tsx
+++ pages/star/[id].tsx
@@ -27,6 +27,27 @@ interface Star {
   mass?: string;
   description?: string;
   wikipedia?: string;
+  wikiTitle?: string;
+  radius?: string;
+  distanceFromEarth?: string;
+  luminosity?: string;
+  parentAstronomicalBody?: string;
+  flattening?: string;
+  spectralClass?: string;
+  apparentMagnitude?: string;
+  absoluteMagnitude?: string;
+  metallicity?: string;
+  density?: string;
+  temperatureCenter?: string;
+  temperatureValuePhotosphere?: string;
+  temperatureCorona?: string;
+  area?: string;
+  volume?: string;
+  perimeter?: string;
+  astronomicSymbolImage?: string;
+  depictedBy?: string;
+  notation?: string;
+  describedBySource?: string;
 }
 
 const Star: React.FC<StarProps> = () => {
@@ -36,23 +57,34 @@ const Star: React.FC<StarProps> = () => {
   const [loading, setLoading] = useState(true);
   const [star, setStar] = useState<Star>();
   const [modelPath, setModelPath] = useState("/models/unknown/scene.gltf"); // New state variable for model path
-  const [starWiki, setStarWiki] = useState<StarWiki>();
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (!id) return;
+    // recherche DBPedia
     searchStarById(id as string)
       .then(async (response) => {
-        const star = (response as any)?.results?.bindings[0];
-        console.log(response);
-        if (star) {
-          for (const key in star) {
-            star[key] = star[key]?.value;
+        const starDBP = (response as any)?.results?.bindings[0];
+        if (starDBP) {
+          for (const key in starDBP) {
+            starDBP[key] = starDBP[key]?.value;
           }
-          setStar(star);
-          // recherche
-          const responseWiki = await searchStarWikidata(star.wikiTitle);
-          console.log(responseWiki);
+          setStar(starDBP);
+          // recherche Wikidata
+          searchStarWikidata(starDBP.wikiTitle).then(
+            async (responseWikidata) => {
+              const starWiki = (responseWikidata as any)?.results?.bindings[0];
+              if (starWiki) {
+                for (const key in starWiki) {
+                  starWiki[key] = starWiki[key]?.value;
+
+                }
+                setStar(starWiki);
+              }
+            }
+          );
         }
+
+        // recherche Wikipedia Image
         getWikipediaImage(id as string).then((imageURL) => {
           if (star) {
             star.imageWikipedia = imageURL!;
@@ -70,7 +102,57 @@ const Star: React.FC<StarProps> = () => {
         console.log(error);
         alert("Star not found");
       });
+  }, [id]);*/
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!id) return;
+  
+      try {
+        const responseDBP = await searchStarById(id as string);
+        const starDBP = (responseDBP as any)?.results?.bindings[0];
+  
+        if (starDBP) {
+          for (const key in starDBP) {
+            starDBP[key] = starDBP[key]?.value;
+          }
+  
+          // Recherche Wikidata
+          const responseWikidata = await searchStarWikidata(starDBP.wikiTitle);
+          const starWiki = (responseWikidata as any)?.results?.bindings[0];
+  
+          if (starWiki) {
+            for (const key in starWiki) {
+              starWiki[key] = starWiki[key]?.value;
+            }
+          }
+  
+          // Recherche Wikipedia Image
+          const imageURL = await getWikipediaImage(id as string);
+  
+          // Fusionner les données
+          const mergedStar = { ...starDBP, ...starWiki, imageWikipedia: imageURL };
+          setStar(mergedStar);
+  
+          // Après avoir fusionné les données, vous pouvez effectuer d'autres actions ici si nécessaire.
+          checkModelPath(id);
+          console.log(imageURL);
+        } else {
+          // Gérer le cas où starDBP n'est pas défini.
+          // Par exemple, afficher un message d'erreur.
+          console.error("Star not found");
+          alert("Star not found");
+        }
+      } catch (error) {
+        // Gérer les erreurs ici
+        console.error(error);
+        alert("Error fetching data");
+      }
+    }
+  
+    fetchData();
   }, [id]);
+  
 
   // Function to simulate checking if the model path exists
   const checkModelPath = (starName) => {
@@ -118,12 +200,35 @@ const Star: React.FC<StarProps> = () => {
                 height={300}
               />
               <span className="flex-grow mr-2">
-                {star?.mass && renderSubSection("Mass", star.mass + " kg")}
+              {star?.distance &&
+                renderSubSection("Distance", star.distance + " km")}
+              {star?.radius && renderSubSection("Radius", star.radius + " km")}
+              {star?.distanceFromEarth && renderSubSection("Distance from Earth", star.distanceFromEarth + " AU")}
+              {star?.luminosity && renderSubSection("Luminosity", star.luminosity + " W")}
+              {star?.parentAstronomicalBody && renderSubSection("Parent Astronomical Body", star.parentAstronomicalBody)}
+              {star?.area && renderSubSection("Area", star.area + " km²")}
+              {star?.volume && renderSubSection("Volume", star.volume + " km³")}
+              {star?.perimeter && renderSubSection("Perimeter", star.perimeter + " km")}
               </span>
             </div>
             <div className="col-span-3 pr-3">
               {star?.description &&
                 renderSection("Description", star.description)}
+              {star?.type && renderSubSection("Type", star.type)}
+              
+              {star?.flattening && renderSubSection("Flattening", star.flattening)}
+              {star?.spectralClass && renderSubSection("Spectral Class", star.spectralClass)}
+              {star?.apparentMagnitude && star?.absoluteMagnitude && renderSubSection("Magnitude : ", "Apparent magnitude ; " + star.apparentMagnitude, "Absolute magnitude ; " + star.absoluteMagnitude,)}
+              {star?.metallicity && renderSubSection("Metallicity", star.metallicity)}
+              {star?.density && renderSubSection("Density", star.density)}
+              {star?.temperatureCenter && renderSubSection("Temperature Center", star.temperatureCenter + " K")}
+              {star?.temperatureValuePhotosphere && renderSubSection("Temperature Photosphere", star.temperatureValuePhotosphere + " K")}
+              {star?.temperatureCorona && renderSubSection("Temperature Corona", star.temperatureCorona + " K")}
+              {star?.astronomicSymbolImage && renderSubSection("Astronomic Symbol Image", star.astronomicSymbolImage)}
+              {star?.depictedBy && renderSubSection("Depicted By", star.depictedBy)}
+              {star?.notation && renderSubSection("Notation", star.notation)}
+              {star?.describedBySource && renderSubSection("Described By Source", star.describedBySource)}
+                
             </div>
           </div>
         </div>
