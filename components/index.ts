535f0ab5b01134d11bc1f5a4344bdf8a33565f7f
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2023-11-22 17:02:13 +0100

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -9,11 +9,8 @@ import GalaxyCanvas from "./GalaxyCanvas";
 import SunCanvas from "./SunCanvas";
 import CustomCanvas from "./CustomCanvas";
 import SaturnCanvas from "./SaturnCanvas";
-<<<<<<< HEAD
 import AboutCanvas from "./AboutCanvas";
-=======
 import Loader from "./Loader";
->>>>>>> 995e020d77324912f2254964e0130769c05607cd
 
 export {
   Loading,
